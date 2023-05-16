2879503d60ee1c295a9cb839c09bdb11f715c491
GurTati <tatiana.gurova.j@gmail.com>
2023-05-16 13:58:09 +0100

    create link to the data and show it on the page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,24 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
+
+
 const BlogDetails = () => {
+    {/* useParams allow us to grad parameters or route parameters from the routes */}
+    const { id } = useParams();
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+
     return (
         <div className="blog-details">
-           <h2>Blog Details</h2>
+            { isPending && <div>Loading ...</div> }
+            { error && <div>{ error }</div> }
+            { blog && 
+              <article>
+                 <h2>{ blog.title }</h2>
+                 <p>Written by { blog.author}</p>
+                 <div>{ blog.body }</div> 
+              </article> }      
         </div>
     );
 }
  
-export default BlobDetatils;
+export default BlogDetails;
